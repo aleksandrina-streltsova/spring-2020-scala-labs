e18f5c76336b3cee61e328120443a7568383f446
aleksandrina-streltsova <aleksandrina.strelczova@mail.ru>
2020-05-10 22:03:43 +0300

    Refactor code structure, replace sortFiles trait with more traits

--- src/main/scala/Program.scala
+++ src/main/scala/Program.scala
@@ -3,6 +3,7 @@ import java.util.stream.Collectors
 
 import cats.{Applicative, Id, Monad}
 import cats.syntax.all._
+import cats.instances.list._
 
 import scala.collection.JavaConverters.asScalaBuffer
 import scala.language.higherKinds
@@ -21,11 +22,14 @@ trait GetFiles[F[_], Dir, File] {
 
 trait Printer[F[_], File] {
   def printName(file: File): F[Unit]
-  def printNames(files: List[File]): F[Unit]
 }
 
-trait SortFiles[F[_], Dir, File] {
-  def sortFiles(dir: Dir, files: List[File]): F[Unit]
+trait GetFileName[F[_], File] {
+  def getFileName(file: File): F[String]
+}
+
+trait MoveFile[F[_], Dir, File] {
+  def moveFile(file: File, dir: Dir): F[File]
 }
 
 class Program[F[_], Dir, File](implicit
@@ -33,7 +37,8 @@ class Program[F[_], Dir, File](implicit
                                mkDir: MkDir[F, Dir],
                                mkFile: MkFile[F, Dir, File],
                                getFiles: GetFiles[F, Dir, File],
-                               sortFiles: SortFiles[F, Dir, File],
+                               getFileName: GetFileName[F, File],
+                               moveFile: MoveFile[F, Dir, File],
                                printer: Printer[F, File]) {
   def run(dir: Dir): F[Unit] = for {
     testDir <- mkDir.mkDir(dir, "test_dir")
@@ -41,42 +46,46 @@ class Program[F[_], Dir, File](implicit
     _ <- mkFile.mkFile(testDir, "bar")
     _ <- mkFile.mkFile(testDir, "baz")
     files <- getFiles.getFiles(testDir)
-    _ <- sortFiles.sortFiles(testDir, files)
-    _ <- printer.printNames(files)
+    _ <- files.traverse(file => printer.printName(file))
+    names <- files.traverse(file => getFileName.getFileName(file))
+    firstLetters = names.map(name => name.head)
+    newDirs <- firstLetters.traverse(letter => mkDir.mkDir(testDir, letter.toString))
+    _ <- files.zip(newDirs).traverse(pair => moveFile.moveFile(pair._1, pair._2))
   } yield ()
 }
 
-class RealFileSystem[F[_] : Applicative] extends MkDir[F, Path] with MkFile[F, Path, Path] with GetFiles[F, Path, Path] with SortFiles[F, Path, Path] {
-  override def mkDir(dir: Path, name: String): F[Path] =
-    Files.createDirectories(dir.resolve(name)).pure[F]
+class RealFileSystem[F[_] : Applicative] extends MkDir[F, Path] with MkFile[F, Path, Path] with GetFiles[F, Path, Path] with GetFileName[F, Path] with MoveFile[F, Path, Path] {
+  override def mkDir(dir: Path, name: String): F[Path] = {
+    val resolved = dir.resolve(name)
+    if (Files.exists(resolved)) {
+      resolved.pure[F]
+    } else {
+      Files.createDirectories(resolved).pure[F]
+    }
+  }
 
-  override def mkFile(dir: Path, name: String): F[Path] =
-    Files.createFile(dir.resolve(name)).pure[F]
+  override def mkFile(dir: Path, name: String): F[Path] = {
+    val resolved = dir.resolve(name)
+    if (Files.exists(resolved) && Files.isRegularFile(resolved)) {
+      resolved.pure[F]
+    } else {
+      Files.createFile(resolved).pure[F]
+    }
+  }
 
   override def getFiles(dir: Path): F[List[Path]] =
     asScalaBuffer(Files.walk(dir).filter(Files.isRegularFile(_)).collect(Collectors.toList[Path])).toList.pure[F]
 
-  private def getFileName(file: Path): String = file.getName(file.getNameCount - 1).toString
+  override def getFileName(file: Path): F[String] = file.getName(file.getNameCount - 1).toString.pure[F]
 
-  private def getFirstLetter(file: Path): String = getFileName(file).substring(0, 1)
-
-  override def sortFiles(dir: Path, files: List[Path]): F[Unit] = {
-    val firstLetters = files.map(getFirstLetter).distinct
-    firstLetters.foreach(letter => mkDir(dir, letter))
-    var dirMap = Map[String, Path]()
-    firstLetters.foreach(letter => dirMap += (letter -> Paths.get(dir.toString, letter)))
-    files.foreach(file =>
-      Files.move(file, dirMap(getFirstLetter(file)).resolve(getFileName(file)), StandardCopyOption.REPLACE_EXISTING))
-  }.pure[F]
+  override def moveFile(file: Path, dir: Path): F[Path] =  Files.move(file, dir.resolve(file.getFileName), StandardCopyOption.REPLACE_EXISTING).pure[F]
 }
 
 class ConsolePathPrinter[F[_] : Applicative] extends Printer[F, Path] {
   override def printName(file: Path): F[Unit] = println(file.getFileName.toString).pure[F]
-
-  override def printNames(files: List[Path]): F[Unit] = println(files.map(_.getFileName.toString).mkString(", ")).pure[F]
 }
 
-object TypeClasse {
+object TypeClasses {
   def main(args: Array[String]): Unit = {
     implicit val fs: RealFileSystem[Id] = new RealFileSystem[Id]
     implicit val printer: ConsolePathPrinter[Id] = new ConsolePathPrinter[Id]
